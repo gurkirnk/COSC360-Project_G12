0ae2125d173c2458e98b54fe936730e8a931e2da
alterednode <75056546+alterednode@users.noreply.github.com>
2026-02-27 09:53:41 -0800

    create "page" components

---
+++ frontend/src/pages/BrowsePage.jsx
@@ -0,0 +1,8 @@
+export default function BrowsePage() {
+  return (
+    <>
+      <h1>Browse</h1>
+      <p>This is the new browse page at /browse.</p>
+    </>
+  );
+}
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+export default function HomePage() {
+  return (
+    <>
+      <h1>Hi!</h1>
+      <p>
+        The layout component can be used to wrap any page in the app and will
+        automatically include the header and footer.
+      </p>
+      <p>
+        Breadcrumbs now follow router paths and update automatically as you
+        navigate between pages.
+      </p>
+    </>
+  );
+}
