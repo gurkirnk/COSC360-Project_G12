03aac6e7d23dc47fce9f3cbb4d89a0b23604911d
alterednode <75056546+alterednode@users.noreply.github.com>
2026-02-27 09:56:31 -0800

    Add TODO

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout({ children, header, footer }) {
   const renderedHeader = header === undefined ? <Header /> : header;
+  //TODO: Make a footer and put it here in place of null
   const renderedFooter = footer === undefined ? null : footer;
   const renderedContent = children ?? <Outlet />;
 
